f204834b7693a26a46dca0e32009a038b0477448
krillavilla <68208879+krillavilla@users.noreply.github.com>
2025-10-15 17:29:07 -0700

    Update MyReads submodule with complete React application

---
+++ gadgets_selling/rn-shop/src/app/(shop)/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Index = () => {
+    return (
+        <View>
+            <Text>Hello!</Text>
+        </View>
+    )
+}
+
+export default Index;
+
+const styles = StyleSheet.create({});
